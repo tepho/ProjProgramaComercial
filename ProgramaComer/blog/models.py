13401bb1dea54b338174588bf075b6ca6cf5d3bd
tepho <stephano_gomes@hotmail.com>
2020-12-14 16:37:34 -0300

    Rodando super usuário e página de add Posts

--- ProgramaComer/blog/models.py
+++ ProgramaComer/blog/models.py
@@ -11,3 +11,6 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateField(auto_now= True)
+
+    def __str__(self):
+        return self.tittle
